247715097b6c5aae3aa03cc2df8ab088a64e64cf
shaswatHota <shaswathota04@gmail.com>
2025-08-20 07:05:27 +0000

    auth middleware name change/fix

--- routes/sectionRoutes.js
+++ routes/sectionRoutes.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { getAllSections, getStudentsBySection, createSection } = require('../controllers/sectionController');
-const auth = require('../middleware/auth');
+const authenticateJwt = require('../middleware/auth');
 
+
+router.use(authenticateJwt);
 // @route   POST api/sections
 // @desc    Create a new section
 
-router.post('/', auth, createSection);
+router.post('/', createSection);
 
 // @route   GET api/sections
 // @desc    Get all available sections
 
-router.get('/', auth, getAllSections);
+router.get('/', getAllSections);
 
 // @route   GET api/sections/:id/students
 // @desc    Get all students for a selected section
 
-router.get('/:id/students', auth, getStudentsBySection);
+router.get('/:id/students', getStudentsBySection);
 
 
 module.exports = router;
